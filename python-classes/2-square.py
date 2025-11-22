01560d7dd78bac9bcbae1b909fc3d373851b7ca7
zarifamammadl4 <zarifamammadli4@gmail.com>
2025-11-22 17:50:25 +0000

    dvvd

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+
+class Square:
+    """Class that defines a square with size validation."""
+
+    def __init__(self, size=0):
+        """Initialize the square with a private size attribute."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
